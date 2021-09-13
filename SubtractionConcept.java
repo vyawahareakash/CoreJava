b8e74f02dcb022db298236d20caf58449fcd7140
vyawahareakash <90264483+vyawahareakash@users.noreply.github.com>
2021-09-13 12:47:49 +0530

    Add files via upload

---
+++ SubtractionConcept.java
@@ -0,0 +1,13 @@
+package com.csi.additionresult;
+
+public class SubtractionConcept {
+public static void main(String[] args) {
+	
+	
+	
+	int i=20;
+	int j=10;
+	int p=i-j;
+	System.out.println("Subtraction Result"+p);
+}
+}
