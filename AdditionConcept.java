3d185df118a7d1303b5bffc51ff24bb93c3e35b1
vyawahareakash <vyawahareakash@gmail.com>
2021-09-13 12:37:08 +0530

    initial commit

---
+++ AdditionConcept.java
@@ -0,0 +1,13 @@
+package com.csi.additionresult;
+
+public class AdditionConcept {
+public static void main(String[] args) {
+	
+	
+	int i=20;
+	int p = 30;
+	int q = p+i;
+	
+	System.out.println("Addiotion Result:"+q);
+}
+}
